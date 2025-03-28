9a1bbe4b0939f8d3b8f2963d4d5848847a46d890
mikeg7895 <maykollgomez050@gmail.com>
2025-03-28 17:36:12 -0500

    Remove __pycache__ directories and alter models

--- backend/apps/monitoring/admin.py
+++ backend/apps/monitoring/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from apps.monitoring.models import AudioFile, Detection, Pulse, Sample, SampleVariable, Specie, Variable
+from apps.monitoring.models import AudioFile, Detection, Sample, SampleVariable, Variable, UnitMeasure, Parameter, Characteristic
 
 
 admin.site.register(AudioFile)
 admin.site.register(Detection)
-admin.site.register(Pulse)
 admin.site.register(Sample)
 admin.site.register(SampleVariable)
-admin.site.register(Specie)
 admin.site.register(Variable)
+admin.site.register(UnitMeasure)
+admin.site.register(Parameter)
+admin.site.register(Characteristic)
--- backend/apps/project/models.py
+++ backend/apps/project/models.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import User
 
 
 class Project(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     description = models.TextField()
+    parent_url = models.CharField(unique=True, max_length=255)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
     guests = models.ManyToManyField(User, related_name='guest_projects', blank=True)
 
@@ -14,6 +15,7 @@ class Project(models.Model):
 
 class Location(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='locations')
+    folder_name = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     point = models.PointField()
 
